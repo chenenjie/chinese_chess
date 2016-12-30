100e8fcb3abf554efa79852f6c0c6841c49fa26b
chenej <chenej@yitop.com>
2016-12-30 17:39:51 +0800

    基本棋子提交

--- src/chess/cannon.rs
+++ src/chess/cannon.rs
@@ -1,11 +1,93 @@
-
-use chess::StepRule;
+use board::get_map;
+use chess::{StepRule, get_map, to_key};
 
 
 struct Cannon;
 
 impl StepRule for Cannon {
     fn get_next_step(&self, side: &Group, my_position: &Position, position: &Position) -> bool {
+        let result_points = Vec::new();
+
+        match my_position {
+            Position{x: x, y: y} => {
+                let map = get_map().lock().unwrap();
+                
+                let mut index = 0;
+                for i in x..0 {
+                    if index == 0 {
+                        if let Some(chess) = map.get(to_key(i, y)){
+                            index = index + 1;
+                        }else{
+                            result_points.push((i, y));
+                        }
+                    } else{
+                        if let Some(chess) = map.get(to_key(i, y)){
+                            if side != chess.group {
+                                result_points.push((i,y))
+                            }
+                        }
+                    }
+                }
+
+                index = 0;
+                for i in x..8 {
+                    if index == 0 {
+                        if let Some(chess) = map.get(to_key(i, y)){
+                            index = index + 1;
+                        }else{
+                            result_points.push((i, y));
+                        }
+                    } else{
+                        if let Some(chess) = map.get(to_key(i, y)){
+                            if side != chess.group {
+                                result_points.push((i,y));
+                            }
+                        }
+                    }
+                }
+                
+                index = 0;
+                for i in y..0 {
+                    if index == 0 {
+                        if let Some(chess) = map.get(to_key(x, i)){
+                            index = index + 1;
+                        }else {
+                            result_points.push((x, i));
+                        }
+                    }else {
+                        if let Some(chess) = map.get(to_key(x, i)){
+                            if side != chess.group {
+                                result_points.push((x, i));
+                            }
+                        }
+                    }
+                }
+
+                index = 0;
+                for i in y..9 {
+                    if index == 0 {
+                        if let Some(chess) = map.get(to_key(x, i)){
+                            index = index + 1;
+                        }else {
+                            result_points.push((x, i));
+                        }
+                    }else {
+                        if let Some(chess) = map.get(to_key(x, i)){
+                            if side != chess.group {
+                                result_points.push((x, i));
+                            }
+                        }
+                    }
+                }
+
+                if result_points.contains(position) {
+                    true
+                }else{
+                    false
+                }
+            }
+        }
         
     }
 }
+
--- src/chess/soldier.rs
+++ src/chess/soldier.rs
@@ -1,10 +1,49 @@
-use chess::StepRule;
-
+use chess::{StepRule, Group, to_key};
+use board::get_map;
 
 struct Soldier;
 
 impl StepRule for Soldier {
     fn get_next_step(&self, side: &Group, my_position: &Position, position: &Position) -> bool {
-        
+        let result_points = Vec::new();
+
+        match my_position {
+            Position{x: x, y: y} => {
+                if side == Group::Red {
+                    result_points.push((x, y+1));
+                    if y > 4 {
+                        result_points.push((x-1, y));
+                        result_points.push((x+1, y));
+                    }
+                }else{
+                    result_points.push((x, y-1));
+                    if y < 5 {
+                        result_points.push((x-1, y));
+                        result_points.push((x+1, y));
+                    }
+                }
+
+                let map = get_map().lock().unwrap();
+                
+                //判断是否己方棋子
+                result_points.iter().filter(|(x, y)| {
+                    if let Some(chess) = map.get(to_key(x, y)){
+                        if chess.group == side {
+                            false
+                        }
+
+                        true
+                    }
+                });
+            }
+        }
+
+
+
+        if result_points.contains(position) {
+            true
+        }else {
+            false
+        }
     }
 }
